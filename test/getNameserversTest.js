534120c3a569128fb8d917e942f8394bf427688d
gkpty <gkardonski@gmail.com>
2020-08-13 02:26:09 -0500

    installed aws and axios dependencies. corrected the getNameservers methods for godaddy and aws

--- test/getNameserversTest.js
+++ test/getNameserversTest.js
@@ -1,3 +1,5 @@
 const getNameServers = require('../lib/nameservers/get')
 
-getNameServers.godaddy('localizehtml.com').then(data=>console.log(data))
+getNameServers.godaddy('localizehtml.com').then(data=>console.log(data)).catch(err=>console.log(err))
+
+getNameServers.aws('supereasyforms.com').then(data=>console.log(data)).catch(err=>console.log(err))
