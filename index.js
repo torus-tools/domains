6e4b42efbeeb941db27f9285f3e46ae89c2f298c
gkpty <gkardonski@gmail.com>
2020-08-13 03:11:17 -0500

    had made a mistake in the index

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const updateNameservers = require('./lib/nameservers/update')
-const deleteRecord = require('./lib/records/delete')
+//const deleteRecord = require('./lib/records/delete')
 
-//updateNameservers
 for(let n in updateNameservers) module.exports[n].updateNameservers = updateNameservers[n]
 
 //addRecord(recordName, recordType)
 //updateRecord(recordName, recordType)
 
 //deleteRecord
-for(let d in deleteRecord) module.exports[a].deleteRecord = deleteRecord[a]
+for(let d in deleteRecord) module.exports[d].deleteRecord = deleteRecord[a]
 
 //updateRecords
 //deleteRecords
